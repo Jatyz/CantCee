2c0ceff657dff64137dd6e05d4bf3449c5074320
TanPohHeng <112920528+TanPohHeng@users.noreply.github.com>
2022-11-02 13:49:32 +0800

    FOV for global illumination with wall logics WIP
    Added:
    - script to handle vector maths
    Updated:
    - Added framework for FOV wall logic for global illumination
    WIP:
    -Reduce FOV dependency on other scripts
    - Global illumination wall logic

---
+++ Src/vectorMathBasic.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//returns the distance of a vector
+double distanceOfVectorR2(
+	double const vectorX,
+	double const vectorY
+);
+
+double dotProductR2
+(
+	double const vector1X,
+	double const vector1Y,
+	double const vector2X,
+	double const vector2Y
+);
+
+//find and return angle between vectors
+double angleBetweenVectorsR2
+(
+	double const vector1X,
+	double const vector1Y,
+	double const vector2X,
+	double const vector2Y
+);
